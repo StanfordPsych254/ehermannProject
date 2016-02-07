f980c0c7531609882c9b38f4ec07e62c9530d6e6
erichermann13 <erichermann13@gmail.com>
2016-02-07 14:56:06 -0800

    Updated a couple thangs

--- HTML/sorites.js
+++ HTML/sorites.js
@@ -20,6 +20,8 @@ var unshuffledFinanceTerms = ["pre-rated stocks", "fixed-rate deduction", "annua
 "fixed-rate mortgage", "home equity", "revolving credit", "vesting", "retirement", "stock options", 
 "inflation", "private equity fund", "interest rate", "Roth IRA", "whole life insurance"];
 
+var specials = ["pre-rated stocks", "fixed-rate deduction", "annualized credit"];
+
 var nEps = epsilonDeviations.length;
 var nVals = valueDeviations.length;
 var nItems = items.length;
@@ -27,129 +29,17 @@ var nQs = unshuffledFinanceTerms.length + personalFinanceQuestions.length;
 var personalFinanceFirst = Math.floor(Math.random()*2);
 
 // For debugging
-nQs = 1;
+// nQs = 1;
 
 var inductivePhrasing = shuffle(["conditional", "relative"])[0];
 
 var shuffledFinanceTerms = shuffle(unshuffledFinanceTerms);
-
-var sds = {
-  "coffee maker": 63.0799749633,
-  "headphones": 79.0068590031,
-  "laptop": 562.8069320736,
-  "sweater": 48.9088453535,
-  "watch": 657.605377432
-}
-
-var means = {
-  "coffee maker": 117.7356754496,
-  "headphones": 119.9895196507,
-  "laptop": 1221.8962172648,
-  "sweater": 83.0482352941,
-  "watch": 722.592738753
-}
-
-function getEps(item, sigs) {
-  if(means[item] == null | sds[item] == null) {alert("ERROR 2");}
-  var raw = sigs*sds[item];
-  return fancyRound(raw);
-}
-
-function getVal(item, sigs) {
-  if(means[item] == null | sds[item] == null) {alert("ERROR 4");}
-  raw = (sigs*sds[item]) + means[item];
-  return fancyRound(raw);
-}
-
-function fancyRound(rawPrice) {
-  if (rawPrice > 200) {
-    var roundPrice = Math.round(rawPrice/50)*50 + ".00";
-  } else if (rawPrice > 20) {
-    var roundPrice = Math.round(rawPrice/10)*10 + ".00";
-  } else if (rawPrice > 2) {
-    var roundPrice = Math.round(rawPrice) + ".00";
-  } else {
-    var roundPrice = Math.round(rawPrice*100)/100;
-  }
-  return "$" + roundPrice;
-}
-
-
-var unshuffledItems = [];
-var unshuffledQTypes = [];
-var unshuffledVals = [];
-var unshuffledEps = [];
-
-
-
-for (var i=0; i<nItems; i++) {
-  //console.log(items[i])
-  for (var j=0; j<nVals; j++) {
-    unshuffledItems.push(items[i]);
-    unshuffledQTypes.push("val");
-    unshuffledVals.push(valueDeviations[j]);
-    //console.log(valueDeviations[j]);
-    unshuffledEps.push("NOPE");
-  }
-  for (var j=0; j<nEps; j++) {
-    unshuffledItems.push(items[i]);
-    unshuffledQTypes.push("eps");
-    unshuffledEps.push(epsilonDeviations[j]);
-    //console.log(epsilonDeviations[j]);
-    unshuffledVals.push("NOPE");
-  }
-}
-
-var unshuffledIndices = [];
-for (var i=0; i<nQs; i++) {
-  unshuffledIndices.push(i);
-}
-var indices = shuffle(unshuffledIndices);
-
-var itemBank = [];
-var qTypes = [];
-var values = [];
-var epsilons = [];
-for (var i=0; i<indices.length; i++) {
-  index = indices[i];
-  itemBank.push(unshuffledItems[index]);
-  var qType = unshuffledQTypes[index];
-  qTypes.push(qType);
-    values.push(unshuffledVals[index]);
-    epsilons.push(unshuffledEps[index]);
-}
 
 
 function caps(a) {return a.substring(0,1).toUpperCase() + a.substring(1,a.length);}
 function uniform(a, b) { return ( (Math.random()*(b-a))+a ); }
 function showSlide(id) { $(".slide").hide(); $("#"+id).show(); }
 function shuffle(v) { newarray = v.slice(0);for(var j, x, i = newarray.length; i; j = parseInt(Math.random() * i), x = newarray[--i], newarray[i] = newarray[j], newarray[j] = x);return newarray;} // non-destructive.
-function article(item) {
-  var art = {
-    "sweater": "a",
-    "headphones": "",
-    "electric kettle": "an",
-    "coffee maker": "a",
-    "watch": "a",
-    "laptop": "a",
-  }
-  if (art[item] == null) {console.log("ERROR 1");}
-  return art[item];
-};
-
-function sg(item) {
-  var singular = {
-    "sweater": true,
-    "headphones": false,
-    "electric kettle": true,
-    "coffee maker": true,
-    "watch": true,
-    "laptop": true,
-  }
-  if (singular[item] == null) {console.log("ERROR 3: " + item);}
-  return singular[item];
-}
-
 function be(singular) { if (singular) {return "is";} else {return "are";} }
 
 $(document).ready(function() {
@@ -174,46 +64,7 @@ var experiment = {
     var startTime = Date.now();
     $("#trialerror").hide();
     showSlide("trial");
-    var item = itemBank[qNumber];
-    var qType = qTypes[qNumber];
-    if (qType == "val") {
-      var sigs = values[qNumber];
-      var dollarAmt = getVal(item, sigs);
-      if (sg(item)) {
-        var statement = "<b>" + caps(article(item)) + " " + item + " that costs " +
-                        dollarAmt + " is expensive.</b>";
-      } else {
-        var statement = "<b>A pair of " + item + " that costs " + dollarAmt +
-                        " is expensive.</b>";
-      }
-    } else if (qType == "eps") {
-      var sigs = epsilons[qNumber];
-      var dollarAmt = getEps(item, sigs);
-      if (inductivePhrasing == "relative") {
-        if (sg(item)) {
-          var statement = "<b>" + caps(article(item)) + " " + item + " that costs " +
-                          dollarAmt + " less than an expensive " + item +
-                          " is also expensive.</b>";
-        } else {
-          var statement = "<b>A pair of " + item + " that costs " + dollarAmt + " less " +
-                          "than an expensive pair of " + item + " is also expensive.</b>";
-        }
-      } else if (inductivePhrasing == "conditional") {
-        if (sg(item)) {
-          var statement = "<b>If " + article(item) + " " + item + " is expensive, " +
-                          " then another " + item + " that costs " +
-                          dollarAmt + " less is also expensive.</b>";
-        } else {
-          var statement = "<b>If a pair of " + item + " is expensive, then another pair of " +
-                          item + " that costs " + dollarAmt + " less is also expensive.</b>";
-        }
-      } else {
-        alert("error 94: " + inductivePhrasing);
-      }
-    } else {
-      console.log("error 6");
-      console.log(qType);
-    }
+    
 
     var financeTerm = "N/A";
     var personalFinanceQuestion = "N/A";
@@ -239,7 +90,7 @@ var experiment = {
     }
 
     $("#statement").html(statement);
-    $('.bar').css('width', ( 100*qNumber/nQs + "%"));
+    $('.bar').css('width', ( 100*qNumber/(nQs+5) + "%"));
     $("#continue").click(function() {
       var responseRaw = $("#form").serialize();
       if (responseRaw.length < 8) {
@@ -252,10 +103,8 @@ var experiment = {
         var rt = endTime - startTime;
         experiment.data.questions.push({
           qNumber:qNumber,
-          qType:qType,
           financeTerm:financeTerm,
           personalFinanceQuestion:personalFinanceQuestion,
-          item:item,
           response:response,
           rt:rt});
         if (qNumber + 1 < nQs) {
@@ -275,7 +124,6 @@ var experiment = {
       }
     });
     //progress bar complete
-    $('.bar').css('width', ( "100%"));
     showSlide("FLQuiz");
     $("#FLQuizerror").hide();
     $("#FLformsubmit").click(function() {
@@ -285,32 +133,49 @@ var experiment = {
         $("#FLQuizerror").show();
       } else {
         // document.write(rawResponse);
+        var score = 0;
         pieces = rawResponse.split("&");
         var q1 = pieces[0].split("=");
         var q1Question = q1[0];
         var q1Answer = q1[1];
+        if (q1Answer == "More+than+102") {
+          score = score + 1;
+        }
 
         var q2 = pieces[1].split("=");
         var q2Question = q2[0];
         var q2Answer = q2[1];
+        if (q2Answer == "Less") {
+          score = score + 1;
+        }
 
         var q3 = pieces[2].split("=");
         var q3Question = q3[0];
         var q3Answer = q3[1];
+        if (q3Answer == "Fall") {
+          score = score + 1;
+        }
 
         var q4 = pieces[3].split("=");
         var q4Question = q4[0];
         var q4Answer = q4[1];
+        if (q4Answer == "True") {
+          score = score + 1;
+        }
 
         var q5 = pieces[4].split("=");
         var q5Question = q5[0];
         var q5Answer = q5[1];
+        if (q5Answer == "False") {
+          score = score + 1;
+        }
 
         experiment.data[q1Question] = q1Answer;
         experiment.data[q2Question] = q2Answer;
         experiment.data[q3Question] = q3Answer;
         experiment.data[q4Question] = q4Answer;
         experiment.data[q5Question] = q5Answer;
+        experiment.data["FLQuizScore"] = score;
 
         experiment.questionaire();
         // setTimeout(function() { turk.submit(experiment.data) }, 1000);
